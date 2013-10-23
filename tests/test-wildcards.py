5c20f63dba8dae4c46a43af6ff40f0103d653718
Alberto Bertogli <albertito@blitiri.com.ar>
2013-10-23 04:17:46 +0100

    tests: Add a wildcards test
    This patch adds a few tests for the wildcard matching.
    
    Signed-off-by: Alberto Bertogli <albertito@blitiri.com.ar>

---
+++ tests/test-wildcards.py
@@ -0,0 +1,44 @@
+"""
+Test the behaviour of the wildcard failure points.
+"""
+
+import fiu
+
+fiu.enable("a:b:c")
+assert fiu.fail("a:b:c")
+
+fiu.enable("a:b:*")
+assert fiu.fail("a:b:c")
+assert fiu.fail("a:b:x")
+assert fiu.fail("a:b:c:d")
+
+fiu.enable("a:b:c:d")
+assert fiu.fail("a:b:c:d")
+
+fiu.disable("a:b:c")
+assert fiu.fail("a:b:c")
+
+fiu.disable("a:b:*")
+assert not fiu.fail("a:b:c")
+assert not fiu.fail("a:b:x")
+assert fiu.fail("a:b:c:d")
+
+fiu.disable("a:b:c:d")
+assert not fiu.fail("a:b:c:d")
+
+
+s = "x"
+for i in range(200):
+    fiu.enable(s + "/*")
+    s += "/x"
+
+s = "x"
+for i in range(200):
+    assert fiu.fail(s + '/asdf')
+    fiu.disable(s + "/*")
+    s += "/x"
+
+fiu.enable("*")
+assert fiu.fail("asdf")
+fiu.disable("*")
+assert not fiu.fail("asdf")
