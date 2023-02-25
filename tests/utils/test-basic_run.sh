a417ce4716e1d9d5db4f20fa86d3eea2f3fb2916
Alberto Bertogli <albertito@blitiri.com.ar>
2023-02-25 11:14:02 +0000

    fiu-run: Fix shell error in deprecated option parsing
    When using the deprecated options (e.g. `-e`), there is a bug where some
    of the bash comparisons are invalid because the variable names are
    incorrect.
    
    This bug has been present since these flags were deprecated in 2012
    (commit c5f4c471).
    
    Thanks to Lynn Kerby <lfkerby@gmail.com> who reported this issue
    and provided a patch for it!
    
    Co-authored-by: Lynn Kerby <lfkerby@gmail.com>

--- tests/utils/test-basic_run.sh
+++ tests/utils/test-basic_run.sh
@@ -12,6 +12,10 @@ set -ex
 ./wrap fiu-run -c "enable name=p1" true
 
 # open() failure.
-! ./wrap fiu-run -x -c "enable name=posix/io/oc/open" cat /dev/null
+( ! ./wrap fiu-run -x -c "enable name=posix/io/oc/open" cat /dev/null )
 
+# Deprecated option: -e.
+( ! ./wrap fiu-run -x -e "posix/io/oc/open" cat /dev/null )
 
+# Deprecated option: -p.
+( ! ./wrap fiu-run -x -e "posix/io/oc/open" -p 1 cat /dev/null )
